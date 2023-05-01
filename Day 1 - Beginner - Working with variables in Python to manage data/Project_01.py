cde18980d6566205b2d290e30c15ef459aa74a30
Bruno Disliler <bruno_disliler@outlook.com>
2023-05-01 18:41:55 -0300

    project 01 - band name generator

---
+++ Day 1 - Beginner - Working with variables in Python to manage data/Project_01.py
@@ -0,0 +1,11 @@
+# Band Name Generator
+
+# 1. Create a greeting for your program.
+print('Welcome to the Band Name Generator')
+# 2. Ask the user for the city that they grew up in.
+city = input('Enter the city you were born in:\n')
+# 3. Ask the user for the name of a pet.
+pet = input('Now, write down your pet´s name:\n')
+# 4. Combine the name of their city and pet and show them their band name.
+print('Your band name is: ' + city + ' ' + pet)
+# 5. Make sure the input cursor shows on a new line:
---
+++ Day 1 - Beginner - Working with variables in Python to manage data/main.py
@@ -0,0 +1,9 @@
+# Print Function Definition
+print('Hello world')
+# input function definition
+input('What is your name?')
+# len and input
+print(len(input('What is your name?')))
+# variables
+name = input('What is your name?')
+print(name)
